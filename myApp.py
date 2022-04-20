f39c265831109cc6f99439fd2f9b5f9984d51abe
jackkatene <73906472+jackkatene@users.noreply.github.com>
2022-04-20 20:53:52 +0200

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
+
+@st.cache
+
+st.image('Logo-KDT-JU.png')
 st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
-st.image('Logo-KDT-JU.png')
+
+#streamlit run sample.py
