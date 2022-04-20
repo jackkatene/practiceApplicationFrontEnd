14cca41ca3599be750f3a093b87a73fc4025728b
jackkatene <73906472+jackkatene@users.noreply.github.com>
2022-04-20 20:43:08 +0200

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -1,6 +1,7 @@
+st.write ("Hello Kiet")
+
 import streamlit as st
 import pandas as pd
-
-st.write ("Hello Kiet")
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-
+st.write("My First Streamlit Web App")
+df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
+st.write(df)
